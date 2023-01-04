841688885885f6d271d01a40dd24b62950c5afa3
sandeep <sandeeppatil776@gmail.com>
2023-01-04 12:17:54 +0530

    sujit updated file

--- src/main/java/in/sandeep/HelloApplication.java
+++ src/main/java/in/sandeep/HelloApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloApplication {
 
+      private Integer Amount=500;
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	 
